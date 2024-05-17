ab2d2004025a3cbe4dd029a4a29f9e1c23507c32
First Git <122622142+thisischrissie@users.noreply.github.com>
2024-05-17 15:23:45 +0100

    created folders for components and pages

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Simple React Home Page</h1>
+      </header>
+      <main>
+        <p>Welcome Home!</p>
+        
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
