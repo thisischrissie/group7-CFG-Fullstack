8b15e1230831748b48abac84b9a5c2873ec14e34
Chrissie <122622142+thisischrissie@users.noreply.github.com>
2024-05-28 15:39:11 +0100

    created new directories for api and webpage

---
+++ api/models/Dog.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+// import PhotoModel from "./Photo";
+
+const DogSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    breed: {
+        type: String,
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    birthday: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    size: {
+        type: String
+    },
+    temperament: {
+        type: Number
+    },
+    bio: {
+        type: String 
+    },
+    images: [
+        {URL: {
+            type: String,
+        },
+        Caption: {
+            type: String
+        }}
+    ]}
+);
+
+const DogModel = mongoose.model("Dog", DogSchema);
+
+export default DogModel;
---
+++ api/routes/OwnerRouter.js
@@ -0,0 +1,45 @@
+
+import express from "express";
+import OwnerModel from "../models/Owner.js";
+const router = express.Router();
+
+
+//GET endpoint to retrieve list of owners
+router.get("/owners", async (request, response) => {
+  try {
+   const owners = await OwnerModel.find({});
+    response.send(owners);
+  } catch (error) {
+    response.status(500).send({ error });
+  }
+});
+
+//POST endpoint to create new user profile
+router.post("/owner", async (request, response) => {
+  const owner = new OwnerModel(request.body);
+
+  try {
+    await owner.save();
+    response.send(owner);
+  } catch (error) {
+    response.status(500).send(error);
+  }
+});
+
+
+//PATCH endpoint to update an existing user profile
+router.patch("/owners/:id", async (request, response) => {
+  try {
+    const article = await OwnerModel.findByIdAndUpdate(
+      request.params.id,
+      request.body
+    );
+    await owner.save();
+    response.send(owner);
+  } catch (error) {
+    response.status(500).send({ error });
+  }
+});
+
+
+export default router;
