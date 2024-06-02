775d4d03c494a202465ec2475c22efd77fce1aa7
cecechang <ceciliachang623@outlook.com>
2024-06-02 01:43:32 +0100

    clean up

--- webapp/src/Hooks/useLogin.jsx
+++ webapp/src/Hooks/useLogin.jsx
@@ -16,7 +16,7 @@ export const useLogin = () => {
       .then(() => {
         const userInfo = { email: email, password: password };
         dispatch(login(JSON.stringify(userInfo)));
-        // dispatch({ type: "LOGIN", payload: userInfo });
+
         console.log("success");
       })
       .catch((e) => {
