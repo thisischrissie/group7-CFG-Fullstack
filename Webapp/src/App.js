212d42086dcee904fb0dcab0c739b9975b7a6c31
Grace Lewis <graceleewis@gmail.com>
2024-06-03 16:31:32 +0100

    updated about us

--- Webapp/src/App.js
+++ Webapp/src/App.js
@@ -1,37 +1,50 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import NavBar from "./components/Nav";
-import DogForm from './components/DogForm'; // Import DogForm component
-import FoundRoute from './pages/FoundRoute';
-import trails from './components/trailArr'; // Import trails data
-import Login from "./pages/Login";
-import HomePage from "./pages/Homepage";
-import AboutUs from "./pages/About";
-import Profile from "./pages/Profile";
-import Breed from './pages/Breeds'; // Import Breed component
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import NavBar from "./components/Nav";
+// import DogForm from './components/DogForm'; // Import DogForm component
+// import FoundRoute from './pages/FoundRoute';
+// import trails from './components/trailArr'; // Import trails data
+// import Login from "./pages/Login";
+// import HomePage from "./pages/Homepage";
+import About from "./pages/About";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import { selectUser } from "./redux/userSlice";
-import { useSelector } from "react-redux";
 
 function App() {
-  const user = useSelector(selectUser);
   return (
-    <Router>
-      <NavBar />
-      {/* previously {user && <NavBar />} on line 20 but the Navbar disappeared on webpage*/}
-      <main>
-        <Routes>
-          <Route path="/" element={user ? <HomePage /> : <Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/breeds" element={<Breed />} />
-          <Route path="/FindRoute" element={<DogForm trails={trails} />} />
-          <Route path="/FoundRoute" element={<FoundRoute />} />
-        </Routes>
-      </main>
-    </Router>
+    <div>
+      <About />
+    </div>
   );
 }
+
+// import Profile from "./pages/Profile";
+// import Breed from './pages/Breeds'; // Import Breed component
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Import bootstrap
+
+
+// import { selectUser } from "./redux/userSlice";
+// import { useSelector } from "react-redux";
+
+// function App() {
+ // const user = useSelector(selectUser);
+ // return (
+   // <Router>
+     // <NavBar />
+    //  {/* previously {user && <NavBar />} on line 20 but the Navbar disappeared on webpage*/}
+//      <main>
+//        <Routes>
+//          <Route path="/" element={user ? <HomePage /> : <Login />} />
+//          <Route path="/login" element={<Login />} />
+//          <Route path="/about" element={<AboutUs />} />
+//          <Route path="/profile" element={<Profile />} />
+//          <Route path="/breeds" element={<Breed />} />
+//          <Route path="/FindRoute" element={<DogForm trails={trails} />} />
+//          <Route path="/FoundRoute" element={<FoundRoute />} />
+//        </Routes>
+//     </main>
+//    </Router>
+//  );
+//}
 
 export default App;
