d6074ec6cc13e785e1aeac26639d6fb5e92ddc09
Grace Lewis <graceleewis@gmail.com>
2024-06-05 21:27:05 +0100

    Removed the commented out parts of the app

--- Webapp/src/App.js
+++ Webapp/src/App.js
@@ -8,33 +8,33 @@ import Login from "./pages/Login";
 import HomePage from "./pages/Homepage";
 import About from "./pages/About";
 import 'bootstrap/dist/js/bootstrap.bundle.min';
- import Profile from "./pages/Profile";
- import Breed from './pages/Breeds'; // Import Breed component
- import 'bootstrap/dist/css/bootstrap.min.css'; // Import bootstrap
+import Profile from "./pages/Profile";
+import Breed from './pages/Breeds'; // Import Breed component
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import bootstrap
 
 
- import { selectUser } from "./redux/userSlice";
- import { useSelector } from "react-redux";
+  import { selectUser } from "./redux/userSlice";
+  import { useSelector } from "react-redux";
 
- function App() {
-  const user = useSelector(selectUser);
+function App() {
+const user = useSelector(selectUser);
   return (
     <Router>
-      <NavBar />
-      {/* previously {user && <NavBar />} on line 20 but the Navbar disappeared on webpage*/}
-      <main>
-        <Routes>
-          <Route path="/" element={user ? <HomePage /> : <Login />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/breeds" element={<Breed />} />
-          <Route path="/FindRoute" element={<DogForm trails={trails} />} />
-          <Route path="/FoundRoute" element={<FoundRoute />} />
-        </Routes>
-     </main>
-    </Router>
-  );
-}
+       <NavBar />
+       {/* previously {user && <NavBar />} on line 20 but the Navbar disappeared on webpage*/}
+       <main>
+         <Routes>
+           {/* <Route path="/" element={user ? <HomePage /> : <Login />} /> */}
+           <Route path="/login" element={<Login />} /> 
+           <Route path="/about" element={<About />} />
+           <Route path="/profile" element={<Profile />} /> 
+           <Route path="/breeds" element={<Breed />} /> 
+           <Route path="/FindRoute" element={<DogForm trails={trails} />} /> 
+           <Route path="/FoundRoute" element={<FoundRoute />} /> 
+        </Routes> 
+      </main>
+     </Router>
+   );
+ }
 
 export default App;
