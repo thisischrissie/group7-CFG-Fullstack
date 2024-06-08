767893fe243337ab301082fc8231f0e8977f38e6
Chrissie <122622142+thisischrissie@users.noreply.github.com>
2024-06-08 20:29:40 +0100

    restored all front-end files

--- Webapp/src/components/UserProfile.jsx
+++ Webapp/src/components/UserProfile.jsx
@@ -1,24 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "react-bootstrap/Card";
-import SmallProfilePic from "./SmallProfilePic";
 import "../styles/UserProfile.css";
 import ProfilePicBio from "./ProfilePicBio";
+import userProfilePic from '../images/ColliemeetsbeagleLogo.png';
+import SmallProfilePic from './SmallProfilePic';
 
 export default function UserProfile() {
-  //is loading
+
+  //is loading 
   const [isLoading, setIsLoading] = useState(true);
 
   const [user, setUser] = useState({});
   //fetching user by ID; test ID endpoint is used line 14
   useEffect(() => {
-    fetch("http://localhost:3001/api/owners/6654ba90507bbb3a0105dc56")
-      .then((response) => response.json())
-      .then((data) => setUser(data))
+    fetch('http://localhost:3001/api/owners/6654ba90507bbb3a0105dc56')
+      .then(response => response.json())
+      .then(data => setUser(data))
       .then(() => setIsLoading(false))
 
-      .catch((error) => console.error("Error fetching users:", error));
+      .catch(error => console.error('Error fetching users:', error));
+
   }, []);
+
 
   return (
     <div className="userProfileRow">
@@ -26,40 +30,37 @@ export default function UserProfile() {
 
         <Card className>
           <Card.Body className>
-            {isLoading ? (
-              <span>loading</span>
-            ) : (
+            {isLoading ? <span>loading</span> :
               <ProfilePicBio
                 name={user.name}
-                src={user.photo}
-                alt="User profile picture"
+                src={userProfilePic}
+                alt='User profile picture'
                 bio={user.bio}
-              />
-            )}
-            <span></span>
+              />}
+            <span>
+
+            </span>
           </Card.Body>
         </Card>
       </div>
       <div className="col-xs-12 col-md-6 ">
-        <Card>
-          <Card.Body>
-            {isLoading ? (
-              <span>loading</span>
-            ) : (
+        <Card >
+          <Card.Body >
+            {isLoading ? <span>loading</span> :
               user.dogs.map((dog) => (
-                <div>
-                  <SmallProfilePic
-                    name={dog.name}
-                    src={dog.images[0]}
-                    alt="dog profile picture"
-                    bio={dog.bio}
-                  />
-                </div>
-              ))
-            )}
+              <div>
+                <ProfilePicBio
+                name={dog.name}
+                src={dog.images[0]}
+                alt='User profile picture'
+                bio={dog.bio}
+                />
+              </div>)) 
+            }
+        
           </Card.Body>
         </Card>
       </div>
     </div>
-  );
-}
+      )
+};
